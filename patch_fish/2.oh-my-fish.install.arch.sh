196f4ad50996db94791d35eec0adf775d8becb58
B00bleaTea <71613062+B00bleaTea@users.noreply.github.com>
2021-04-02 19:03:58 +0000

    Add files via upload

---
+++ patch_fish/2.oh-my-fish.install.arch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+# installing fish
+git clone https://github.com/oh-my-fish/oh-my-fish/
+oh-my-fish/bin/install
+
+echo 'please reload your terminal'
---
+++ patch_fish/3.omf.install.arch.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+
+# installing packages
+omf install archlinux
+omf install lambda
