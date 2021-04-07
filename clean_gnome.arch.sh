be80891562fc5f55f1af3b24d89bf96da6d134dc
B00bleaTea <71613062+B00bleaTea@users.noreply.github.com>
2021-04-07 01:20:50 +0000

    Update clean_gnome.arch.sh

--- clean_gnome.arch.sh
+++ clean_gnome.arch.sh
@@ -3,10 +3,10 @@
 # removing not needed packages
 sudo pacman -R yelp gnome-calendar gnome-logs gnome-weather gnome-user-share gnome-user-docs gnome-system-monitor gnome-software gnome-remote-desktop gnome-photos gnome-maps gnome-getting-started-docs gnome-font-viewer gnome-documents gnome-contacts gnome-clocks gnome-characters gnome-boxes gnome-books gnome-backgrounds epiphany
 
-# cleaning up
-while [ ! "$(pacman -Qdtq)" ]
+# cleaning up (will run until nothing is found or until 5 times)
+for _ in {1...5}
 do
-    sudo pacman -Rsn "$(pacman -Qdtq)"
+    sudo pacman -Rsn $(pacman -Qdtq)
 done
 
 # reinstalling fuse2
