5638c4fa7b59b7f9e095240ce084c0391ee7bde4
B00bleaTea <71613062+B00bleaTea@users.noreply.github.com>
2021-04-14 21:21:32 +0000

    Add files via upload

--- i3/packages.arch.sh
+++ i3/packages.arch.sh
@@ -4,23 +4,28 @@ aur_helper_install="paru -S --needed"
 sudo_command="sudo"
 
 echo 'installing python and pip'
+sleep 2
 $aur_helper_install python3 python-pip
 
 
 echo 'installing important packages'
-$aur_helper_install i3-gaps xorg i3status i3lock-color i3blocks xorg-xinit rofi terminator
+sleep 2
+$aur_helper_install i3-gaps xorg i3status i3lock-color i3blocks xorg-xinit rofi terminator thunar
 
 
 echo 'installing extra packages'
+sleep 2
 $aur_helper_install autotiling-git picom ttf-font-awesome lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance fish git
 
 
 echo 'installing fish'
+sleep 2
 chsh -s /usr/bin/fish
 $sudo_command chsh -s /usr/bin/fish
 
 
 echo 'enabling lightdm'
+sleep 2
 $sudo_command systemctl enable lightdm
 
 echo 'Done!'
