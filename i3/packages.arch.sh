3ae62f3d4a072a610049492f544e7ea2c85de165
B00bleaTea <71613062+B00bleaTea@users.noreply.github.com>
2021-04-14 19:38:59 +0000

    Add files via upload

--- i3/packages.arch.sh
+++ i3/packages.arch.sh
@@ -6,14 +6,21 @@ sudo_command="sudo"
 echo 'installing python and pip'
 $aur_helper_install python3 python-pip
 
+
 echo 'installing important packages'
 $aur_helper_install i3-gaps xorg i3status i3lock-color i3blocks xorg-xinit rofi terminator
 
+
 echo 'installing extra packages'
-$aur_helper_install autotiling-git picom ttf-font-awesome lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance
+$aur_helper_install autotiling-git picom ttf-font-awesome lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance fish git
+
+
+echo 'installing fish'
+chsh -s /usr/bin/fish
+$sudo_command chsh -s /usr/bin/fish
+
 
 echo 'enabling lightdm'
 $sudo_command systemctl enable lightdm
 
 echo 'Done!'
-exit 0
---
+++ patch_fish/5.fish.modify.path.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/fish
+
+fish_add_path /home/$USER/.local/bin
+fish_add_path /usr/local/bin/
