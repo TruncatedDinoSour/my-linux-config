38a9da2bf021a10158ca975ac7a9d1a9cd125cd8
B00bleaTea <71613062+B00bleaTea@users.noreply.github.com>
2021-04-15 23:42:24 +0000

    Add files via upload

--- transfer_configs.sh
+++ transfer_configs.sh
@@ -35,6 +35,7 @@ cp -r /home/ari/.config/rofi i3/_.config
 cp -r /home/ari/.config/scripts i3/_.config
 cp -r /home/ari/.config/terminator i3/_.config
 cp -r /home/ari/.config/Thunar i3/_.config
+cp -r /home/ari/.config/htop/ i3/_.config
 
 cp /usr/share/rofi/themes/onedark.rasi i3/_.config/rofi
 cp /etc/xdg/reflector/reflector.conf reflector.conf
